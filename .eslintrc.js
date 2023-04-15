1cadd8fcdff24060ed62c7d53304b3bbb41e2182
CasimirColas <casimir.colas@gmail.com>
2023-04-15 12:21:04 +0200

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:react/jsx-runtime",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["react"],
+  rules: {},
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+## Basic NextJs template
+
+PS: In development you might want to remove the build option from Husky to have faster commits
+
+by Casimir Colas
